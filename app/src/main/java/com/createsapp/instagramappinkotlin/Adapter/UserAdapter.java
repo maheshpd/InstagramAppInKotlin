b1a50bbdf756c46d9e0a4fe03eae1a872a620175
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-03-07 22:28:09 +0530

    Still Pending

---
+++ app/src/main/java/com/createsapp/instagramappinkotlin/Adapter/UserAdapter.java
@@ -0,0 +1,5 @@
+package com.createsapp.instagramappinkotlin.Adapter;
+
+class UserAdapter (private var mContext:Context,
+private var mUser:List<>){
+        }
---
+++ app/src/main/java/com/createsapp/instagramappinkotlin/Model/User.kt
@@ -0,0 +1,19 @@
+package com.createsapp.instagramappinkotlin.Model
+
+class User {
+    private var username: String = ""
+    private var fullname: String = ""
+    private var bio: String = ""
+    private var image: String = ""
+    private var uid: String = ""
+
+    constructor()
+
+    constructor(username: String, fullname: String, bio: String, image: String, uid: String) {
+        this.username = username
+        this.fullname = fullname
+        this.bio = bio
+        this.image = image
+        this.uid = uid
+    }
+}
